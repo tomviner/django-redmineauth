8b6fbfcd4d5dc652ed54cf70b639d06d966df7ef
Tom Viner <tom.viner@u-dox.com>
2012-11-29 10:57:22 +0000

    make compatible with django1.5 swappable User models

--- redmineauth/backends.py
+++ redmineauth/backends.py
@@ -2,8 +2,13 @@ import re
 import requests
 
 from django.conf import settings
-from django.contrib.auth.models import User
 
+try:
+    from django.contrib.auth import get_user_model
+except ImportError:
+    from django.contrib.auth.models import User
+else:
+    User = get_user_model()
 
 
 
